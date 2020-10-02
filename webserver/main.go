c1749991a6ecfa6de244f21cb045f21fc122b598
diffnest <327985860@qq.com>
2020-10-02 23:31:21 +0800

    text

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> hello, ^^^^^****&&&&uuuiiiiAAAABBBBBCCCCDDDD!</h1>")
+	io.WriteString(w, "<h1> hello, $$$$$$^^^^^****&&&&uuuiiiiAAAABBBBBCCCCDDDD!</h1>")
 }
 
 func main() {
